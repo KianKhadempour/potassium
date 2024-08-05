f696f5886cf3030346377326dbd9c6b460e0d452
KianKhadempour <kiankhadempour@gmail.com>
2024-08-04 18:24:50 -0700

    Finish assembler
    Also refactors the parser and lexer to be functions instead of structs

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,5 +1,5 @@
-use lexer::Lexer;
-use parser::Parser;
+use lexer::lex;
+use parser::parse;
 use token::ParseError;
 
 mod instruction;
@@ -7,21 +7,188 @@ mod lexer;
 mod parser;
 mod token;
 
-struct Assembler {
-    lexer: Lexer,
-}
+pub fn assemble(input: &str) -> Result<Vec<[u8; 4]>, ParseError> {
+    let tokens = lex(input)?;
+    let instructions = parse(tokens)?;
 
-impl Assembler {
-    pub fn new(input: String) -> Self {
-        Assembler {
-            lexer: Lexer::new(input),
+    let mut output = vec![];
+
+    for instruction in instructions {
+        match instruction {
+            instruction::Instruction::HLT => output.push([0, 0, 0, 0]),
+            instruction::Instruction::LOAD(reg, int) => {
+                output.push([1, reg, (int >> 8) as u8, int as u8]);
+            }
+            instruction::Instruction::ADD(reg1, reg2, reg3) => {
+                output.push([2, reg1, reg2, reg3]);
+            }
+            instruction::Instruction::SUB(reg1, reg2, reg3) => {
+                output.push([3, reg1, reg2, reg3]);
+            }
+            instruction::Instruction::MUL(reg1, reg2, reg3) => {
+                output.push([4, reg1, reg2, reg3]);
+            }
+            instruction::Instruction::DIV(reg1, reg2, reg3) => {
+                output.push([5, reg1, reg2, reg3]);
+            }
+            instruction::Instruction::JMP(reg) => {
+                output.push([6, reg, 0, 0]);
+            }
+            instruction::Instruction::JMPF(reg) => {
+                output.push([7, reg, 0, 0]);
+            }
+            instruction::Instruction::JMPB(reg) => {
+                output.push([8, reg, 0, 0]);
+            }
+            instruction::Instruction::EQ(reg1, reg2) => {
+                output.push([9, reg1, reg2, 0]);
+            }
+            instruction::Instruction::NEQ(reg1, reg2) => {
+                output.push([10, reg1, reg2, 0]);
+            }
+            instruction::Instruction::GT(reg1, reg2) => {
+                output.push([11, reg1, reg2, 0]);
+            }
+            instruction::Instruction::LT(reg1, reg2) => {
+                output.push([12, reg1, reg2, 0]);
+            }
+            instruction::Instruction::GTQ(reg1, reg2) => {
+                output.push([13, reg1, reg2, 0]);
+            }
+            instruction::Instruction::LTQ(reg1, reg2) => {
+                output.push([14, reg1, reg2, 0]);
+            }
+            instruction::Instruction::JEQ(reg) => {
+                output.push([15, reg, 0, 0]);
+            }
+            instruction::Instruction::JNEQ(reg) => {
+                output.push([16, reg, 0, 0]);
+            }
         }
     }
+    Ok(output)
+}
 
-    pub fn assemble(&mut self) -> Result<Vec<[u8; 4]>, ParseError> {
-        let tokens = self.lexer.lex()?;
-        let instructions = Parser::new(tokens).parse()?;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        todo!();
+    #[test]
+    fn test_assemble_hlt() {
+        let expected_output = vec![[0, 0, 0, 0]];
+
+        assert_eq!(assemble("HLT"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_load() {
+        let expected_output = vec![[1, 0, 1, 244]];
+
+        assert_eq!(assemble("LOAD $0 #500"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_add() {
+        let expected_output = vec![[2, 0, 1, 2]];
+
+        assert_eq!(assemble("ADD $0 $1 $2"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_sub() {
+        let expected_output = vec![[3, 0, 1, 2]];
+
+        assert_eq!(assemble("SUB $0 $1 $2"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_mul() {
+        let expected_output = vec![[4, 0, 1, 2]];
+
+        assert_eq!(assemble("MUL $0 $1 $2"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_div() {
+        let expected_output = vec![[5, 0, 1, 2]];
+
+        assert_eq!(assemble("DIV $0 $1 $2"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_jmp() {
+        let expected_output = vec![[6, 0, 0, 0]];
+
+        assert_eq!(assemble("JMP $0"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_jmpf() {
+        let expected_output = vec![[7, 0, 0, 0]];
+
+        assert_eq!(assemble("JMPF $0"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_jmpb() {
+        let expected_output = vec![[8, 0, 0, 0]];
+
+        assert_eq!(assemble("JMPB $0"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_eq() {
+        let expected_output = vec![[9, 0, 1, 0]];
+
+        assert_eq!(assemble("EQ $0 $1"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_neq() {
+        let expected_output = vec![[10, 0, 1, 0]];
+
+        assert_eq!(assemble("NEQ $0 $1"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_gt() {
+        let expected_output = vec![[11, 0, 1, 0]];
+
+        assert_eq!(assemble("GT $0 $1"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_lt() {
+        let expected_output = vec![[12, 0, 1, 0]];
+
+        assert_eq!(assemble("LT $0 $1"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_gtq() {
+        let expected_output = vec![[13, 0, 1, 0]];
+
+        assert_eq!(assemble("GTQ $0 $1"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_ltq() {
+        let expected_output = vec![[14, 0, 1, 0]];
+
+        assert_eq!(assemble("LTQ $0 $1"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_jeq() {
+        let expected_output = vec![[15, 0, 0, 0]];
+
+        assert_eq!(assemble("JEQ $0"), Ok(expected_output));
+    }
+
+    #[test]
+    fn test_assemble_jneq() {
+        let expected_output = vec![[16, 0, 0, 0]];
+
+        assert_eq!(assemble("JNEQ $0"), Ok(expected_output));
     }
 }
--- src/assembler/parser.rs
+++ src/assembler/parser.rs
@@ -2,181 +2,163 @@ use crate::assembler::instruction::Instruction;
 
 use super::token::{ParseError, Token};
 
-pub struct Parser {
-    input: Vec<Token>,
-}
+pub fn parse(input: Vec<Token>) -> Result<Vec<Instruction>, ParseError> {
+    use crate::assembler::token::Token as T;
+    use crate::opcode::Opcode as O;
+    let mut pos = 0;
+    let len = input.len();
 
-impl Parser {
-    pub fn new(input: Vec<Token>) -> Self {
-        Parser { input }
-    }
+    let mut output = vec![];
 
-    pub fn parse(&self) -> Result<Vec<Instruction>, ParseError> {
-        use crate::assembler::token::Token as T;
-        use crate::opcode::Opcode as O;
-        let mut pos = 0;
-        let len = self.input.len();
-
-        let mut output = vec![];
-
-        while pos < len {
-            match &self.input[pos] {
-                Token::Op(opcode) => match (
-                    opcode,
-                    self.input.get(pos + 1),
-                    self.input.get(pos + 2),
-                    self.input.get(pos + 3),
-                ) {
-                    (O::HLT, _, _, _) => {
-                        output.push(Instruction::HLT);
-                        pos += 1;
-                    }
-                    (O::LOAD, Some(T::Register(reg)), Some(T::IntegerOperand(int)), _) => {
-                        output.push(Instruction::LOAD(*reg, *int));
-                        pos += 3;
-                    }
-                    (
-                        O::ADD,
-                        Some(T::Register(reg1)),
-                        Some(T::Register(reg2)),
-                        Some(T::Register(reg3)),
-                    ) => {
-                        output.push(Instruction::ADD(*reg1, *reg2, *reg3));
-                        pos += 4;
-                    }
-                    (
-                        O::SUB,
-                        Some(T::Register(reg1)),
-                        Some(T::Register(reg2)),
-                        Some(T::Register(reg3)),
-                    ) => {
-                        output.push(Instruction::SUB(*reg1, *reg2, *reg3));
-                        pos += 4;
-                    }
-                    (
-                        O::MUL,
-                        Some(T::Register(reg1)),
-                        Some(T::Register(reg2)),
-                        Some(T::Register(reg3)),
-                    ) => {
-                        output.push(Instruction::MUL(*reg1, *reg2, *reg3));
-                        pos += 4;
-                    }
-                    (
-                        O::DIV,
-                        Some(T::Register(reg1)),
-                        Some(T::Register(reg2)),
-                        Some(T::Register(reg3)),
-                    ) => {
-                        output.push(Instruction::DIV(*reg1, *reg2, *reg3));
-                        pos += 4;
-                    }
-                    (O::JMP, Some(T::Register(reg)), _, _) => {
-                        output.push(Instruction::JMP(*reg));
-                        pos += 2;
-                    }
-                    (O::JMPF, Some(T::Register(reg)), _, _) => {
-                        output.push(Instruction::JMPF(*reg));
-                        pos += 2;
-                    }
-                    (O::JMPB, Some(T::Register(reg)), _, _) => {
-                        output.push(Instruction::JMPB(*reg));
-                        pos += 2;
-                    }
-                    (O::EQ, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
-                        output.push(Instruction::EQ(*reg1, *reg2));
-                        pos += 3;
-                    }
-                    (O::NEQ, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
-                        output.push(Instruction::NEQ(*reg1, *reg2));
-                        pos += 3;
-                    }
-                    (O::GT, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
-                        output.push(Instruction::GT(*reg1, *reg2));
-                        pos += 3;
-                    }
-                    (O::LT, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
-                        output.push(Instruction::LT(*reg1, *reg2));
-                        pos += 3;
-                    }
-                    (O::GTQ, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
-                        output.push(Instruction::GTQ(*reg1, *reg2));
-                        pos += 3;
-                    }
-                    (O::LTQ, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
-                        output.push(Instruction::LTQ(*reg1, *reg2));
-                        pos += 3;
-                    }
-                    (O::JEQ, Some(T::Register(reg)), _, _) => {
-                        output.push(Instruction::JEQ(*reg));
-                        pos += 2;
-                    }
-                    (O::JNEQ, Some(T::Register(reg)), _, _) => {
-                        output.push(Instruction::JNEQ(*reg));
-                        pos += 2;
-                    }
-                    _ => {
-                        return Err(ParseError::InvalidOpcodeError(
-                            "sequence of opcodes could not be parsed to instruction".to_owned(),
-                        ))
-                    }
-                },
+    while pos < len {
+        match &input[pos] {
+            Token::Op(opcode) => match (
+                opcode,
+                input.get(pos + 1),
+                input.get(pos + 2),
+                input.get(pos + 3),
+            ) {
+                (O::HLT, _, _, _) => {
+                    output.push(Instruction::HLT);
+                    pos += 1;
+                }
+                (O::LOAD, Some(T::Register(reg)), Some(T::IntegerOperand(int)), _) => {
+                    output.push(Instruction::LOAD(*reg, *int));
+                    pos += 3;
+                }
+                (
+                    O::ADD,
+                    Some(T::Register(reg1)),
+                    Some(T::Register(reg2)),
+                    Some(T::Register(reg3)),
+                ) => {
+                    output.push(Instruction::ADD(*reg1, *reg2, *reg3));
+                    pos += 4;
+                }
+                (
+                    O::SUB,
+                    Some(T::Register(reg1)),
+                    Some(T::Register(reg2)),
+                    Some(T::Register(reg3)),
+                ) => {
+                    output.push(Instruction::SUB(*reg1, *reg2, *reg3));
+                    pos += 4;
+                }
+                (
+                    O::MUL,
+                    Some(T::Register(reg1)),
+                    Some(T::Register(reg2)),
+                    Some(T::Register(reg3)),
+                ) => {
+                    output.push(Instruction::MUL(*reg1, *reg2, *reg3));
+                    pos += 4;
+                }
+                (
+                    O::DIV,
+                    Some(T::Register(reg1)),
+                    Some(T::Register(reg2)),
+                    Some(T::Register(reg3)),
+                ) => {
+                    output.push(Instruction::DIV(*reg1, *reg2, *reg3));
+                    pos += 4;
+                }
+                (O::JMP, Some(T::Register(reg)), _, _) => {
+                    output.push(Instruction::JMP(*reg));
+                    pos += 2;
+                }
+                (O::JMPF, Some(T::Register(reg)), _, _) => {
+                    output.push(Instruction::JMPF(*reg));
+                    pos += 2;
+                }
+                (O::JMPB, Some(T::Register(reg)), _, _) => {
+                    output.push(Instruction::JMPB(*reg));
+                    pos += 2;
+                }
+                (O::EQ, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
+                    output.push(Instruction::EQ(*reg1, *reg2));
+                    pos += 3;
+                }
+                (O::NEQ, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
+                    output.push(Instruction::NEQ(*reg1, *reg2));
+                    pos += 3;
+                }
+                (O::GT, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
+                    output.push(Instruction::GT(*reg1, *reg2));
+                    pos += 3;
+                }
+                (O::LT, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
+                    output.push(Instruction::LT(*reg1, *reg2));
+                    pos += 3;
+                }
+                (O::GTQ, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
+                    output.push(Instruction::GTQ(*reg1, *reg2));
+                    pos += 3;
+                }
+                (O::LTQ, Some(T::Register(reg1)), Some(T::Register(reg2)), _) => {
+                    output.push(Instruction::LTQ(*reg1, *reg2));
+                    pos += 3;
+                }
+                (O::JEQ, Some(T::Register(reg)), _, _) => {
+                    output.push(Instruction::JEQ(*reg));
+                    pos += 2;
+                }
+                (O::JNEQ, Some(T::Register(reg)), _, _) => {
+                    output.push(Instruction::JNEQ(*reg));
+                    pos += 2;
+                }
                 _ => {
                     return Err(ParseError::InvalidOpcodeError(
-                        "instruction must start with an opcode".to_owned(),
+                        "sequence of opcodes could not be parsed to instruction".to_owned(),
                     ))
                 }
+            },
+            _ => {
+                return Err(ParseError::InvalidOpcodeError(
+                    "instruction must start with an opcode".to_owned(),
+                ))
             }
         }
-
-        return Ok(output);
     }
+
+    return Ok(output);
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::{
-        assembler::{instruction::Instruction, parser::Parser, token::Token, ParseError},
+        assembler::{instruction::Instruction, token::Token, ParseError},
         opcode::Opcode,
     };
 
     #[test]
-    fn test_parser() {
-        use crate::opcode::Opcode;
-
+    fn test_parse() {
         let input = vec![
             Token::Op(Opcode::LOAD),
             Token::Register(0),
             Token::IntegerOperand(500),
         ];
-
-        let parser = Parser::new(input);
-
         let expected_output = vec![Instruction::LOAD(0, 500)];
 
-        assert_eq!(parser.parse(), Ok(expected_output))
+        assert_eq!(parse(input), Ok(expected_output))
     }
     #[test]
-    fn test_parser_failure_1() {
+    fn test_parse_failure_1() {
         let input = vec![Token::Register(0), Token::IntegerOperand(500)];
 
-        let parser = Parser::new(input);
-
         assert_eq!(
-            parser.parse(),
+            parse(input),
             Err(ParseError::InvalidOpcodeError(
                 "instruction must start with an opcode".to_owned(),
             ))
         )
     }
     #[test]
-    fn test_parser_failure_2() {
+    fn test_parse_failure_2() {
         let input = vec![Token::Op(Opcode::LOAD), Token::IntegerOperand(500)];
 
-        let parser = Parser::new(input);
-
         assert_eq!(
-            parser.parse(),
+            parse(input),
             Err(ParseError::InvalidOpcodeError(
                 "sequence of opcodes could not be parsed to instruction".to_owned(),
             ))
